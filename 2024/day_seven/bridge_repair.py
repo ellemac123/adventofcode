ad6c7e204375ee5f00fc740496e5abb32c0f623e
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-10 18:16:14 +0000

    solution for part 1 of day 7

--- 2024/day_seven/bridge_repair.py
+++ 2024/day_seven/bridge_repair.py
@@ -1,10 +1,10 @@
-from csv import DictReader
-OPERATORS = [
-    '-', 
-    '+', 
-    '/', 
-    '*'
-]
+"""
+https://adventofcode.com/2024/day/7
+
+solution for part 1:  303766880536
+"""
+
+from itertools import product
 
 def create_shape(input_file): 
     new_list = []
@@ -16,11 +16,66 @@ def create_shape(input_file):
 
     return new_list
 
+def multiply(number_one, number_two): 
+    return number_one * number_two
+
+def evaluate_string(total, values_array): 
+    flag = True    
+    i = 0 
+    while flag: 
+        if len(values_array) >= 3: 
+            if not values_array[i + 1].isdigit(): 
+                if values_array[i + 1] == '+': 
+                    values_array[i] = sum([values_array[i], values_array[i+2]])
+                else: 
+                    values_array[i] = multiply(values_array[i], values_array[i+2])
+                
+                values_array.pop(i+2)
+                values_array.pop(i+1)
+        else: 
+            flag = False
+
+    if values_array[0] == total: 
+        return True
+    return False
+
+def run(shaped_data):
+    """
+    Given a list, call check_match to see if there is a way to
+    create the total from the list
+
+    if so add to count, sum and return
+    """
+    totals = []
+    for i in shaped_data: 
+        sum_ = int(i[0])
+        values = [int(num) for num in i[1]]
+        combos = (list(product('+*', repeat=len(values)-1)))
+        for iter in range(len(combos)): 
+            """loop through all the combos"""
+            final = []
+            operator_string = combos[iter]
+            # order values
+            for i in range(len(values)):
+                final.append(values[i])
+                if i < len(operator_string): 
+                    final.append(operator_string[i])
+
+            if evaluate_string(sum_, final): 
+                totals.append(sum_)
+                break
+
+    return sum(totals)
+
+
+
 if __name__ == "__main__": 
     file_input = []
-    with open('tiny_input.txt', 'r') as file: 
+    with open('input.txt', 'r') as file: 
         for line in file.readlines(): 
             file_input.append(line.strip())
 
     shaped_list = create_shape(file_input)
-    print(shaped_list)
+    totals = run(shaped_list)
+
+    print('solution for part 1: ', totals)
