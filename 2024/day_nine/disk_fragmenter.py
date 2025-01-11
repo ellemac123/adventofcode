f0b10a9d4781122e55fb211ea7c068639e58bb2d
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-11 17:30:48 +0000

    not quite right

--- 2024/day_nine/disk_fragmenter.py
+++ 2024/day_nine/disk_fragmenter.py
@@ -3,8 +3,8 @@ https://adventofcode.com/2024/day/9
 
 part one solution:
 
-2333133121414131402
 """
+import re
 def convert_to_list(file_line):
     """
     Convert the list into the files and free space indicator
@@ -13,20 +13,59 @@ def convert_to_list(file_line):
     conversion_string = []
     id = 0
     i = 0
-    stop = len(file_line)
-    while i < stop:
+    while i < len(file_line):
         for a in range(int(file_line[i])):
-            conversion_string.append(id)
-        if i+1 < stop:
+            conversion_string.append(str(id))
+        if i+1 < len(file_line):
             for b in range(int(file_line[i + 1])):
                 conversion_string.append('.')
         id = id + 1
         i += 2
-
+    print(conversion_string)
     return conversion_string
 
+def move_file_blocks(space_indicator_list):
+    pattern = r'\.'
+    ra = int(len(space_indicator_list) / 2) + 2
+    print('here')
+    for i in range(1, ra):
+        # traversing backwards in the list
+        # check if the number is a digit
+        # if not, continue on
+        if space_indicator_list[-i].isdigit():
+            stringed = ''.join(space_indicator_list)
+            match = re.search(pattern, stringed)
+            try:
+                loc = match.start()
+            except:
+                break
+            val = space_indicator_list[-i]
+
+
+            space_indicator_list[loc] = val
+            space_indicator_list[-i] = 'X'
+
+    return space_indicator_list
+
+def product_time(final_list):
+    biggy = []
+    for i in final_list:
+        if i.isdigit():
+            biggy.append(i)
+
+    prods = []
+    for v in range(len(biggy)):
+        f = v * int(biggy[v])
+        prods.append(f)
+
+    return sum(prods)
+
+
+
 if __name__ == "__main__":
-    with open('tiny_input.txt', 'r') as input_file:
+    with open('input.txt', 'r') as input_file:
         line = input_file.readline()
 
-    convert_to_list(list(line))
+    converted_list = convert_to_list(list(line))
+    space_list = move_file_blocks(converted_list)
+    print(product_time(space_list))
