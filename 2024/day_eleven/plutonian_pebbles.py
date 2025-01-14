65a4d59cb82927ca35410284ede06fe2eba68a06
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-14 20:43:14 +0000

    solved speed challenge for parts one and two!

--- 2024/day_eleven/plutonian_pebbles.py
+++ 2024/day_eleven/plutonian_pebbles.py
@@ -3,68 +3,72 @@ https://adventofcode.com/2024/day/11
 
 Part One:
 How many stones will you have after blinking 25 times?
+
 Solution for Part One:  231278
+initial solution = time =  0:00:00.263549
+tiny input = time =  0:00:00.066682
+Using a Dict over a list = time =  0:00:00.003368
+
+
+Part Two with speed improvement:  274229228071551
+time =  0:00:00.112909
+
 """
-from functools import lru_cache
+import datetime
+from collections import Counter, defaultdict
+
 
 def format_input_string(input_string):
     """
     Given an input_string from the file,
-
     return it stripped, split, and turned into integers.
     """
     return [int(num) for num in input_string.strip().split(' ')]
 
-def blink(stones):
-    """
-    If the stone is engraved with the number 0,
-        it is replaced by a stone engraved with the number 1.
-    If the stone is engraved with a number that has an even number of digits,
-        it is replaced by two stones.
-        The left half of the digits are engraved on the new left stone,
-        and the right half of the digits are engraved on the new right stone.
-        (The new numbers don't keep extra leading zeroes:
-            1000 would become stones 10 and 0.)
-    If none of the other rules apply,
-        the stone is replaced by a new stone;
-        the old stone's number multiplied by 2024 is engraved on the new stone.
-    """
-    plutonian_stones = []
-
-    for stone in stones:
-        if stone == 0:
-            plutonian_stones.append(1)
-        else:
-            char_stone = len(str(stone))
-            if char_stone % 2 == 0:
-                # halfway = 10 to the power of floor divided digits
-                halfway = 10 ** (char_stone // 2)
-                right = stone // halfway
-                left = stone % halfway
-
-                plutonian_stones.extend([right, left])
-            else:
-                plutonian_stones.append(stone*2024)
-
-    return plutonian_stones
-
 def run(input_list, blinks):
     """
-    Given a list of blinks, transform the input_list that many times
+    This was a crazy one - originally used a loop with a list, but it
+    grew exponentially so have swapped to a dict since order isn't important here
     """
-    morphed = input_list
-    for _ in range(blinks):
-        morphed = blink(morphed)
+    input_list = Counter(input_list)
+    for n in range(blinks):
+        iteration_stone = defaultdict(int)
+        for stone in input_list.keys():
+            val = input_list[stone]
+            if stone == 0:
+                iteration_stone[1] += val
+            else:
+                char_stone = len(str(stone))
+                if char_stone % 2 == 0:
+                    # halfway = 10 to the power of floor divided digits
+                    halfway = 10 ** (char_stone // 2)
+                    right = stone // halfway
+                    left = stone % halfway
 
-    return len(morphed)
+                    iteration_stone[right] += val
+                    iteration_stone[left] += val
+                else:
+                    iteration_stone[stone * 2024] += val
+
+        input_list = iteration_stone
+
+    sum = 0
+    for a in input_list.keys():
+        sum = sum + input_list[a]
+    return sum
+
+
 
 if __name__ == "__main__":
     with open('input.txt', 'r') as f:
         input_text = f.read()
 
     input_list = format_input_string(input_text)
-    # blinks = 25
-    # print('Solution for Part One: ', run(input_list, blinks))
-
     blinks = 75
-    print('Solution for Part Two: ', run(input_list, blinks))
+
+    tim = datetime.datetime.now()
+
+    print('Part Two with speed improvement: ', run(input_list, blinks))
+
+    tim2 = datetime.datetime.now()
+    print('time = ', tim2 - tim)
