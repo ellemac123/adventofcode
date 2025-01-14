2a011d0fced39a66da620e0b2410d87a72d357a0
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-14 21:08:53 +0000

    solution to part two

--- 2020/day_one/report_repair.py
+++ 2020/day_one/report_repair.py
@@ -1,6 +1,9 @@
+"""
+https://adventofcode.com/2020/day/1
 
+"""
 
-def run(input_list):
+def sum(input_list):
     """
         For item in input list,
         subtract by 2020, abs value it, and see if its in the list
@@ -12,10 +15,31 @@ def run(input_list):
         if searcher in input_list:
             return searcher * val
 
+def triple_sum(input_list):
+    """
+    Loop through a
+
+    Loop through b
+
+    if 2020 - b - a in list
+        return the product of a b and c
+        else return -1
+    """
+    for a in input_list:
+        sub = 2020 - a
+        for b in input_list:
+            c = sub - b
+            if c in input_list:
+                return c * a * b
+    return -1
+
 if __name__ == "__main__":
     input = []
     with open('input.txt', 'r') as f:
         for line in f:
             input.append(int(line))
 
-    print('Solution for part one: ', run(input))
+    print('Solution for part one: ', sum(input))
+
+    print('Solution for part two: ', triple_sum(input))
+
