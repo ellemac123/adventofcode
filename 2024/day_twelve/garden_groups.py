2a960374f759b56fbb09d22f6279e2751e98f9a6
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-15 20:25:33 +0000

    working solution for part one

--- 2024/day_twelve/garden_groups.py
+++ 2024/day_twelve/garden_groups.py
@@ -1,5 +1,7 @@
 """
 https://adventofcode.com/2024/day/12
+
+the answer for part one is:  1477762
 """
 from collections import Counter, defaultdict
 
@@ -10,68 +12,92 @@ DIRECTIONS = [
     (0, -1),  # left
 ]
 
-
-def get_area_for_each(input_list): 
+def get_area_for_each(input_list):
     """
     Given an input list containing rows of letters
 
-    get a count of how many letters there are in each 
+    get a count of how many letters there are in each
 
     return area count
     """
     letters_area = Counter()
-    for line in input_list: 
+    for line in input_list:
         letters_area.update(line)
 
     return letters_area
 
-def calc_perimiter(input_list, unique_letters): 
-    """
-    Given an input_list
-    and a list of unique letters
 
-    loop through the unique letters list
-    loop through each row
-        check if the letter == unique letter we are searching for
-        if yes
-            check each side of the letter
-            if the side ISNT the same letter
-            incrememnt count by 1
+def traverse_letters(input_list):
+    letters_remaining = set(get_area_for_each(input_list))
+    """   
+    loop through the list
+    for each row, check each value and see if its not a digit
+    
+    if not - find all touching it
+    """
+    finals = []
+    for row_num, row in enumerate(input_list):
+        if any(letter in letters_remaining for letter in row):
+            for col_num, val in enumerate(row):
+                if val in letters_remaining:
+                    _, perim, area = find_touching(val, input_list, row_num, col_num)
+                    finals.append([val, perim, area])
+    return finals
+
+
+
+def find_touching(letter, input_list, row_num, col_num):
+    """
+    Given a letter, input_list and location
+
+    letter in position == letter
+        increase area by 1
+
+    check all 4 directions
+        if the letter is touching in any direction, traverse that direction
         else
-            continue
+            increment parameter by 1
 
+    after all that set it to its numerical ordenance
+
+    return updated input_list, perimeter, area
     """
-    perimiter = defaultdict(int)
+    area = 0
+    perimeter = 0
+    set_letter = ord(letter) - 97
 
-    for letter in unique_letters: 
-        perimiter[letter] = 0
-        for row_num, row in enumerate(input_list): 
-            if letter in row: 
-                for col_num, row_value in enumerate(row):
-                    if row_value == letter:
-                        for direction in DIRECTIONS: 
-                            try:
-                                check_row = row_num + direction[0]
-                                check_col = col_num + direction[1]
-                                # check if the next spot is out of bounds
-                                check = (check_row < 0 or check_col < 0)
-                                if row_value != input_list[check_row][check_col] or check:
-                                    perimiter[row_value] += 1
-                            except IndexError: 
-                                # if there is an out of bounds next to it that counts as a perimiter!
-                                perimiter[row_value] += 1
-    print(perimiter)
-    return perimiter
+    if letter == input_list[row_num][col_num]:
+        area = area + 1
+        input_list[row_num][col_num] = set_letter
+        for direction in DIRECTIONS:
+            check_row = row_num + direction[0]
+            check_col = col_num + direction[1]
+            check = (check_row < 0 or check_col < 0 or check_row >= len(input_list) or check_col >= len(input_list[0]))
+            if check:
+                perimeter = perimeter + 1
+            elif input_list[check_row][check_col] != letter and input_list[check_row][check_col] != set_letter:
+                perimeter = perimeter + 1
+            elif input_list[check_row][check_col] == letter:
+                input_list, searched_area, searched_perimeter = find_touching(letter, input_list, check_row, check_col)
+                area = area + searched_area
+                perimeter = perimeter + searched_perimeter
 
+    return input_list, area, perimeter
 
-def calculate_area_params(area_dict, parameter_dict): 
-    sum = 0
-    for letter in area_dict.keys(): 
-        sq_footage = area_dict[letter] * parameter_dict[letter]
-        sum = sum + sq_footage
+def sum_vals(input):
+    """
+    Return the product of all the sums of the lines
+    """
+    totals = defaultdict(int)
+    sum_ = 0
 
-    return sum 
+    for line in input:
+        totals[line[0]] += line[1] * line[2]
 
+    for keys in totals.keys():
+       sum_ = sum_ + totals[keys]
+
+    return sum_
 
 
 if __name__ == '__main__':
@@ -80,8 +106,9 @@ if __name__ == '__main__':
         for line in f.readlines(): 
             input_list.append(list(line.strip()))
 
-    area = get_area_for_each(input_list)
-    # keys should contain all the unique letters in the counter
-    parameters = calc_perimiter(input_list, area.keys())
 
-    print('Answer for part one: ', calculate_area_params(area, parameters))
+
+    final_traverse = traverse_letters(input_list)
+    sum_values = sum_vals(final_traverse)
+
+    print('the answer for part one is: ', sum_values)
