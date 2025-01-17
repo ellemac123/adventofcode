820ac7bccb6264685da95b9d6d1806054ad39ef5
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-17 20:24:11 +0000

    solution for part two!

--- 2021/day_two/dive.py
+++ 2021/day_two/dive.py
@@ -1,12 +1,19 @@
 """
 https://adventofcode.com/2021/day/2
 answer for part one:  1427868
+answer for part two:  1568138742
+
 """
 DIRECTIONS = {
     'forward': (1, 0), 
     'down': (0, 1),
     'up': (0, -1)
 
+}
+
+AIM_MAP = {
+    'up': 1, 
+    'down': -1
 }
 
 def calculate_depth(depth_map): 
@@ -25,6 +32,27 @@ def calculate_depth(depth_map):
     
     return position
 
+def calculate_aim(depth_map): 
+    """
+    Aim Calculations: 
+        down X increases your aim by X units.
+        up X decreases your aim by X units.
+        forward X does two things:
+        It increases your horizontal position by X units.
+        It increases your depth by your aim multiplied by X.
+    """
+    aim = 0
+    position = [0,0]
+
+    for line in depth_map: 
+        calc = int(line[1])
+        if line[0] in AIM_MAP.keys():
+            aim += AIM_MAP[line[0]] * calc
+        else: 
+            position[0] += calc
+            position[1] += (aim * calc)
+    
+    return position
 
 def run(): 
     depth_map = []
@@ -33,8 +61,10 @@ def run():
             depth_map.append(line.strip().split(' '))
     
     final_position = calculate_depth(depth_map)
-
     print('answer for part one: ', final_position[0]*final_position[1])
+
+    final_aim = calculate_aim(depth_map)
+    print('answer for part two: ', abs(final_aim[0]*final_aim[1]))
 
 
 if __name__ == "__main__": 
