4946aa0ba9eab82f0d706f8a1af43adbf016e274
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-17 19:23:10 +0000

    solution for part 2

--- 2023/day_four/scratchcards.py
+++ 2023/day_four/scratchcards.py
@@ -2,8 +2,9 @@
 https://adventofcode.com/2023/day/4
 
 the answer for part one is:  21213
+the answer for part two is:  8549735
 """
-
+from collections import defaultdict, Counter
 
 def matches(scratcher_list): 
     totals = 0
@@ -21,10 +22,43 @@ def matches(scratcher_list):
     
     return totals
 
-        
+
+def scratchcard_copies(scratcher_list): 
+    """
+    Create a counter of the scratch cards
+
+    loop through the cards and increment their count
+
+    return dictionary of copies
+    """
+    copies = {i:1 for i in range(len(scratcher_list))}
+
+    for row_iteration, row in enumerate(scratcher_list): 
+        match_counter = 1
+        for number in row[0]: 
+            if number in row[1]: 
+                copies[row_iteration + match_counter] += 1*copies[row_iteration]
+                match_counter += 1
+    
+    return copies
+
+
+def count_scratchers(scratch_dict): 
+    """
+    Sum scratchcards in dictionary
+    """
+    sum = 0 
+    for i in scratch_dict.keys():
+        sum += scratch_dict[i]
+
+    return sum
 
 
 if __name__ == "__main__": 
+    """
+    Format the list and run the code for part one and two
+    """
+
     scratcher_list = []
 
     with open('input.txt', 'r') as f: 
@@ -37,3 +71,7 @@ if __name__ == "__main__":
             scratcher_list.append([first, sec])
 
     print('the answer for part one is: ', matches(scratcher_list))
+
+    # part two
+    scratch_counts = scratchcard_copies(scratcher_list)
+    print('the answer for part two is: ', count_scratchers(scratch_counts))
