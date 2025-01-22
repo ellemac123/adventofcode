f0fc667d816531c6745f6dabb37669611a834146
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-22 09:02:34 +0000

    starting part two for day four

--- 2020/day_four/passport_processing.py
+++ 2020/day_four/passport_processing.py
@@ -4,17 +4,18 @@ https://adventofcode.com/2020/day/4
 The solution for part one is: 196
 """
 from collections import defaultdict
+import re
 
-PASSPORT_INFORMATION = (
-    "byr", # birth year
-    "iyr", # Issue Year
-    "eyr", # expiration year
-    "hgt", # (Height)
-    "hcl", #(Hair Color)
-    "ecl", #(Eye Color)
-    "pid", #(Passport ID)
+PASSPORT_INFORMATION = {
+    "byr": re.compile(r'\d\d\d\d'), # birth year
+    "iyr": re.compile(r'\d\d\d\d'), # Issue Year
+    "eyr": re.compile(r'\d\d\d\d'), # expiration year
+    "hgt": re.compile(r'\d\d'), # (Height)
+    "hcl": re.compile(r'\d\d\d\d'), #(Hair Color)
+    "ecl": re.compile(r'\d\d\d\d'), #(Eye Color)
+    "pid": re.compile(r'\d\d\d\d'), #(Passport ID)
     # "cid", -- we can ignore this one
-)
+}
 
 
 def check_passport_validity(passport_dict): 
@@ -26,6 +27,7 @@ def check_passport_validity(passport_dict):
     return valid_passports
 
 def format_file(raw_input): 
+    
     passports = []
     for entry in raw_input: 
         passport = defaultdict()
@@ -37,6 +39,21 @@ def format_file(raw_input):
         passports.append(passport)
 
     return passports
+
+def check_passport_validity_full(passport_dict): 
+    valid_passports = 0
+    for pp in passport_dict: 
+        if all(key in pp.keys() for key in PASSPORT_INFORMATION):
+            flag = True
+            for key in PASSPORT_INFORMATION.keys(): 
+                if not re.match(PASSPORT_INFORMATION[key], pp[key]):
+                    flag = False
+
+            if flag:
+                valid_passports +=1
+    
+    return valid_passports
+
 
 def run(): 
     input = []
@@ -53,11 +70,13 @@ def run():
     formatted_passports = format_file(input)
 
     total_valid_passports = check_passport_validity(formatted_passports)
+    total_valid_passports_two = check_passport_validity_full(formatted_passports)
 
-    return total_valid_passports
+    return total_valid_passports, total_valid_passports_two
 
 
 if __name__ == "__main__": 
-    valid_passports = run()
+    valid_passports, valid_passports_two = run()
 
     print('The solution for part one is:', valid_passports)
+    print('The solution for part two is:', valid_passports_two)
