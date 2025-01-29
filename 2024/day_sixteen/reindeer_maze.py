54113718eddeb791d9b21e60c525d1c9b765128a
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-29 19:17:29 +0000

    create the map

--- 2024/day_sixteen/reindeer_maze.py
+++ 2024/day_sixteen/reindeer_maze.py
@@ -2,6 +2,31 @@
 https://adventofcode.com/2024/day/16
 """
 
+DIRECTIONS = [
+    (1, 0), 
+    (0, 1), 
+    (-1, 0), 
+    (0, -1),
+]
+
+def create_map(): 
+    """
+    Read the input file and add to a map list
+    return map
+    """
+    map = []
+    with open('tiny_input.txt', 'r') as f: 
+        for line in f.readlines(): 
+            map.append(list(line.strip()))
+    
+    return map
+
+def run(): 
+    """
+    Run this all! 
+    """
+    map = create_map()
+    print(map)
 
 if __name__ == "__main__": 
-    print('setup')
+    run()
