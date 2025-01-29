d5265e4544a8761c1fd89400cde64085e88e7e43
ellemac123 <laura.macaluso.12@cnu.edu>
2025-01-29 20:22:54 +0000

    start of a recursive function

--- 2024/day_sixteen/reindeer_maze.py
+++ 2024/day_sixteen/reindeer_maze.py
@@ -21,13 +21,40 @@ def find_start_location(map):
     
     return start, end
 
+
+def recursive_move(map, position): 
+    for direction in DIRECTIONS: 
+        if map[position[0]+ direction[0]][position[1]+direction[1]] == '.': 
+            # set the location to a "-"
+            # move to the next posision using recursion
+            map[position[0]][position[1]] = '^'
+            position = (position[0]+ direction[0], position[1]+direction[1])
+            map[position[0]][position[1]] = 'S'
+            val = recursive_move(map, position)
+            if val:
+                return val + 1
+        elif map[position[0]+ direction[0]][position[1]+direction[1]] == 'E': 
+            return 1
+
+
 def move(map):
     """
 
     """ 
+    moves = -1
     start, end = find_start_location(map)
     print(start, end)
+    
+    position = start[:]
+    print(position)
 
+    total = recursive_move(map, position)
+    print(total)
+
+        
+    
+    for a in map: 
+        print(a)
 
 def create_map(): 
     """
