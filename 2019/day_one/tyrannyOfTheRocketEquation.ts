e81711e67146026e1342a9d2b2c6ec8424cba824
ellemac123 <laura.macaluso.12@cnu.edu>
2025-03-01 19:11:41 +0000

    reading a file in typescript!

--- 2019/day_one/tyrannyOfTheRocketEquation.ts
+++ 2019/day_one/tyrannyOfTheRocketEquation.ts
@@ -1,19 +1,24 @@
 import * as fs from 'node:fs';
 
+function readFileFunction(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        fs.readFile('input.txt', 'utf-8', (err, data) => {
+            if(err) {
+                console.error('cannot read local file')
+                reject('cannot read local file')
+            }
+            resolve(data)
+        });
+    }
+)}
 
-
-function calculateFuel(fileInput) {
-
+async function main() {
+    try {
+        const fileData = await readFileFunction();
+        console.log(fileData);
+    } catch(err) {
+        console.error('error reading file');
+    }
 }
 
-
-
-const file = fs.readFile('input.txt', 'utf-8', (err, data) => {
-    if(err) {
-        console.error('cannot read local file')
-        return
-    }
-    return data
-});
-
-console.log(file)
+main();
