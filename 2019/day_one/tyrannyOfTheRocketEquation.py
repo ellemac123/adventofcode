4d66a9e710f2cea853eeba597697457cf7950d91
ellemac123 <laura.macaluso.12@cnu.edu>
2025-03-01 18:30:06 +0000

    part two solution python

--- 2019/day_one/tyrannyOfTheRocketEquation.py
+++ 2019/day_one/tyrannyOfTheRocketEquation.py
@@ -2,6 +2,7 @@
 Day one advent of code in python - https://adventofcode.com/2019/day/1
 
 the answer for part one is: 3311492
+the answer for part two is: 4964376
 """
 import math 
 
@@ -12,17 +13,24 @@ def find_mass(inputlist):
     then subtracting 2
     """
     total = 0
+    part_two_total = 0
 
     for i in inputlist: 
-        total += (math.floor(i/3) - 2)
+        value = (math.floor(i/3) - 2)
+        total += value
 
-    return total
+        while value > 0: 
+            part_two_total += value
+            value = (math.floor(value/3) - 2)
+
+    return total, part_two_total
 
 if __name__ == "__main__": 
     file_input = []
     with open('input.txt', 'r') as inputFile: 
         file_input = [int(line.strip()) for line in inputFile]
 
-    total_mass = find_mass(file_input)
+    total_mass, part_two_total_mass = find_mass(file_input)
 
     print(f"the answer for part one is: {total_mass}")
+    print(f"the answer for part two is: {part_two_total_mass}")
--- 2019/day_one/tyrannyOfTheRocketEquation.ts
+++ 2019/day_one/tyrannyOfTheRocketEquation.ts
@@ -1,9 +1,19 @@
 import * as fs from 'node:fs';
 
+
+
+function calculateFuel(fileInput) {
+
+}
+
+
+
 const file = fs.readFile('input.txt', 'utf-8', (err, data) => {
     if(err) {
         console.error('cannot read local file')
         return
     }
-    console.log(`File Content: ${data}`)
+    return data
 });
+
+console.log(file)
